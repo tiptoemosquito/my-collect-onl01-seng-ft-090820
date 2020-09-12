ac4b56e1caf99f32a6145e44e4ea1263f3d7dcde
Christina Howard <clhoward33@icloud.com>
2020-09-12 20:06:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,11 +2,8 @@ def my_collect(array)
   i = 0
   collection = []
   while i < array.length
-    collection << yield array[i]
+    collection << yield (array[i)]
     i += 1
   end
 collection
-
-
-
 end
